4a673db80a42fe1c65786e64d047582061b8ffd4
syy1125 <syy1125@hotmail.com>
2016-10-22 22:26:05 -0700

    Movement script
    Basic movement script implemented. The palaceholder character can now move horizontally.
    Jumping script is currently under construction.

---
+++ Renaissance Simulator/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+    public float HorizontalSpeed;
+    public float VerticalSpeed;
+
+    public Rigidbody2D ThisBody;
+    public Collider2D ThisCollider;
+    public Collider2D GroundCollider;
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        ThisBody.AddForce(new Vector2(Input.GetAxis("Horizontal") * HorizontalSpeed, 0));
+
+        if (ThisCollider.IsTouchingLayers() && Input.GetAxis("Vertical") > 0)
+        {
+            ThisBody.AddForce(new Vector2(VerticalSpeed, 0), ForceMode2D.Impulse);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+	    
+	}
+}
