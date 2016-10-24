8ec79f8322d9d602a1c50dd7dab20f32ad54284b
Jack Liu <jackxuanliu@gmail.com>
2016-10-23 18:50:36 -0700

    put scripts in proper location and fixed curly brackets

---
+++ Renaissance Simulator/Assets/Scripts/MenuScripts/MenuController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuItemPrefab;
+    public GameObject menuItemPrefab2;
+    public Dictionary<GameObject, Action> menuItems = new Dictionary<GameObject, Action>();
+    public Dictionary<GameObject, Action> menuItems2 = new Dictionary<GameObject, Action>();
+    private Dictionary<GameObject, Action> currentMenu = null;
+
+    Vector2 positionOfFirstMenuItem = new Vector2(-7.16f, -0.73f);
+    // Use this for initialization
+    void Start()
+    {
+        menuItems.Add(Instantiate(menuItemPrefab), () =>
+            { //make individual menu item objects later
+                Debug.Log("Menu Item 1-1 Pressed");
+            }); 
+        menuItems.Add(Instantiate(menuItemPrefab), () =>
+            {
+                Debug.Log("Menu Item 1-2 Pressed");
+
+            });
+        menuItems.Add(Instantiate(menuItemPrefab), () =>
+            {
+                Debug.Log("Menu Item 1-3 Pressed");
+
+            });
+        menuItems.Add(Instantiate(menuItemPrefab), () =>
+            {
+                Debug.Log("Menu Item 1-4 Pressed");
+                switchMenus(menuItems2);
+
+            });
+        menuItems2.Add(Instantiate(menuItemPrefab2), () =>
+            { 
+                Debug.Log("Menu Item 2-1 Pressed");
+            }); 
+        menuItems2.Add(Instantiate(menuItemPrefab2), () =>
+            {
+                Debug.Log("Menu Item 2-2 Pressed");
+
+            });
+        menuItems2.Add(Instantiate(menuItemPrefab2), () =>
+            {
+                Debug.Log("Menu Item 2-3 Pressed");
+
+            });
+        menuItems2.Add(Instantiate(menuItemPrefab2), () =>
+            {
+                Debug.Log("Menu Item 2-4 Pressed");
+                switchMenus(menuItems);
+
+            });
+        switchMenus(menuItems);
+
+    }
+
+    public void doMenuAction(GameObject menuItem)
+    {
+        currentMenu[menuItem]();
+    }
+
+    public void switchMenus(Dictionary<GameObject, Action> menuToSwitchTo)
+    {
+
+        float delayOfAnimation = 0;
+        if (currentMenu != null)
+        {
+            foreach (GameObject menuItem in currentMenu.Keys)
+            {
+                MenuItemController menuItemController = menuItem.GetComponent<MenuItemController>();
+                menuItemController.StopAllCoroutines();
+                menuItemController.acceptingMouseActions = false;
+                StartCoroutine(menuItemController.hideWithDelay(delayOfAnimation));
+
+                delayOfAnimation += 0.15f; //amount of time between starting each movement (for cascading effect)
+            }
+            delayOfAnimation *= 0.4f; //overlap
+
+        }
+
+
+        Vector2 menuItemPosition = positionOfFirstMenuItem;
+        foreach (GameObject menuItem in menuToSwitchTo.Keys)
+        {
+            MenuItemController menuItemController = menuItem.GetComponent<MenuItemController>();
+            menuItemController.init(menuItemPosition, this);
+            menuItem.transform.position = menuItemController.hidePosition;
+            menuItemController.StopAllCoroutines();
+            StartCoroutine(menuItemController.unhideWithDelay(delayOfAnimation));
+
+            menuItemPosition = new Vector2(menuItemPosition.x, menuItemPosition.y - 1.0f);
+            delayOfAnimation += 0.15f; //amount of time between starting each movement (for cascading effect)
+        }
+        currentMenu = menuToSwitchTo;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+}
---
+++ Renaissance Simulator/Assets/Scripts/MenuScripts/MenuItemController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuItemController : MonoBehaviour
+{
+    private Vector2 originalPosition;
+    private Vector2 finalPosition;
+    public Vector2 hidePosition;
+    public bool acceptingMouseActions = false;
+    private int hoveringTimer = 0;
+    private MenuController parentMenuController;
+    private float interpolation = 0;
+    // Use this for initialization
+    public void init(Vector2 originalPosition, MenuController parentMenuController)
+    {
+        this.originalPosition = originalPosition;
+        this.parentMenuController = parentMenuController;
+        finalPosition = new Vector2(originalPosition.x + 1.0f, originalPosition.y);
+        hidePosition = new Vector2(-9f - 0.5f * transform.localScale.x, originalPosition.y);
+
+    }
+
+    void Update()
+    {
+        if (acceptingMouseActions)
+        {
+            if (hoveringTimer > 0)
+            {
+                interpolation += 0.1f;
+                hoveringTimer--;
+            }
+            else
+            {
+                interpolation -= 0.1f;
+            }
+            interpolation = Mathf.Clamp(interpolation, 0, 1f);
+            transform.position = Vector2.Lerp(originalPosition, finalPosition, interpolation * interpolation);
+        }
+        else
+        {
+            interpolation = 0;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (acceptingMouseActions == true)
+        {
+            hoveringTimer = 8;
+        }
+    }
+
+
+    void OnMouseDown()
+    {
+        if (acceptingMouseActions == true)
+        {
+            parentMenuController.doMenuAction(gameObject);
+        }
+    }
+
+    public IEnumerator hideWithDelay(float delay)
+    {
+        Vector2 currentPosition = transform.position;
+        yield return new WaitForSeconds(delay);
+        for (float i = 1; i > 0.05; i -= 0.05f)
+        {
+            transform.position = Vector2.Lerp(hidePosition, currentPosition, i * i);
+            yield return null;
+        }
+    }
+
+    public IEnumerator unhideWithDelay(float delay)
+    {
+        Vector2 currentPosition = transform.position;
+        yield return new WaitForSeconds(delay);
+        for (float i = 1; i > 0; i -= 0.05f)
+        {
+            transform.position = Vector2.Lerp(originalPosition, currentPosition, i * i);
+            yield return null;
+        }
+        acceptingMouseActions = true;
+    }
+}
