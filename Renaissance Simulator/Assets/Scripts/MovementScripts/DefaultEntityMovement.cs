2e3b35dbd3d73e5012fbcf81154a99865b464eda
syy1125 <syy1125@hotmail.com>
2016-11-23 19:10:08 -0800

    Better ground layer masks
    The script now accepts an array of layer indices and turns them into a layer mask, instead of requesting the layer mask directly.

--- Renaissance Simulator/Assets/Scripts/MovementScripts/DefaultEntityMovement.cs
+++ Renaissance Simulator/Assets/Scripts/MovementScripts/DefaultEntityMovement.cs
@@ -1,41 +1,44 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.Assertions.Comparers;
+﻿using System;
+using UnityEngine;
 
 public abstract class DefaultEntityMovement : AbstractEntityMovement
 {
     private const float DefualtAirborneSpeedMultiplier = 0.25F;
     private const float DefaultJumpForce = 2F;
 
-    public float AirborneForceMultiplier;
+    public float AirborneSpeedMultiplier;
     public float JumpForce;
 
-    void Start()
+    protected float JumpLaunchDirection;
+
+    protected new virtual void Start()
     {
-        if (AirborneForceMultiplier < 0)
+        base.Start();
+
+        if (AirborneSpeedMultiplier < 0)
         {
-            AirborneForceMultiplier = DefualtAirborneSpeedMultiplier;
+            AirborneSpeedMultiplier = DefualtAirborneSpeedMultiplier;
         }
         if (JumpForce < 0)
         {
             JumpForce = DefaultJumpForce;
         }
+        JumpLaunchDirection = 0;
     }
 
     protected abstract float GetHorizontalInput();
 
     protected abstract float GetVerticalInput();
 
-    protected bool IsReversingDirection()
-    {
-        float HorizontalInput = GetHorizontalInput();
-        return (HorizontalInput < 0 && ThisBody.velocity.x > 0) || (HorizontalInput > 0 && ThisBody.velocity.x < 0);
-    }
-
     protected override float GetHorizontalMove()
     {
         return GetHorizontalInput() * HorizontalSpeedCap *
-               (!IsGrounded() && IsReversingDirection() ? AirborneForceMultiplier : 1f);
+               (IsReversingDirection() ? AirborneSpeedMultiplier : 1f);
+    }
+
+    protected override float GetJumpForce()
+    {
+        return JumpForce;
     }
 
     protected override bool GetJump()
@@ -43,8 +46,16 @@ public abstract class DefaultEntityMovement : AbstractEntityMovement
         return IsGrounded() && GetVerticalInput() > 0;
     }
 
-    protected override float GetJumpForce()
+    protected bool IsReversingDirection()
     {
-        return JumpForce;
+        float HorizontalInput = GetHorizontalInput();
+        return !IsGrounded() &&
+               ((HorizontalInput < 0 && JumpLaunchDirection > 0) || (HorizontalInput > 0 && JumpLaunchDirection < 0));
+    }
+
+    protected override void OnJump()
+    {
+        base.OnJump();
+        JumpLaunchDirection = ThisBody.velocity.x;
     }
 }
