1a26c69d39becc76064fe466b27714037397bb7d
syy1125 <syy1125@hotmail.com>
2016-12-03 21:02:59 -0800

    Basic damage scripts and fire controller script
    Implemented basic functionality for character health and damage.
    Added a fire controller script that will damage the player if the player does not move far enough away.

---
+++ Renaissance Simulator/Assets/Scripts/EntityScripts/CombatScripts/DamagableEntity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class DamagableEntity : MonoBehaviour
+{
+    private const int DefaultMaxHealth = 100;
+    public int MaxHealth;
+    public int DamageReduction;
+    private int _health;
+
+    public delegate void DeathAction(GameObject gameObject);
+
+    /// <summary>
+    /// The script that handles death behaviour.
+    /// <br />
+    /// It is a function that takes one GameObject parameter, which is the game object that is dying.
+    /// </summary>
+    public DeathAction OnDeath;
+
+    private void DefaultDeathAction(GameObject gameObject)
+    {
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // Initialize to default values if needed.
+        if (MaxHealth <= 0)
+        {
+            MaxHealth = DefaultMaxHealth;
+        }
+        _health = MaxHealth;
+
+        if (OnDeath == null)
+        {
+            OnDeath = DefaultDeathAction;
+        }
+    }
+
+    /// <summary>
+    /// Directly damages the character's health pool, ignores all delegate calculations.
+    /// </summary>
+    /// <param name="change">The amount of health points to take away</param>
+    public void ChangeHealth(int change)
+    {
+        _health += change;
+        print(_health);
+
+        if (_health < 0)
+        {
+            OnDeath(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Damages the entity.
+    /// <br />
+    /// All functions overriding ApplyDamage should use ChangeHealth to change the health of the character.
+    /// </summary>
+    /// <param name="damageStrength">The strength of the damage</param>
+    public virtual void ApplyDamage(int damageStrength)
+    {
+        int actualDamage = damageStrength - DamageReduction;
+        ChangeHealth(actualDamage < 0 ? 0 : actualDamage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public int Health
+    {
+        get { return _health; }
+    }
+}
---
+++ Renaissance Simulator/Assets/Scripts/TutorialSceneScripts/FireControllerScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireControllerScript : MonoBehaviour
+{
+    public GameObject Player;
+
+    public float StartPoint;
+    public float PropagationSpeed;
+    private float _currentFront;
+
+    // Use this for initialization
+    void Start()
+    {
+        _currentFront = StartPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentFront += PropagationSpeed * Time.deltaTime;
+
+        // Null check
+        if (Player == null || Player.transform == null) return;
+
+        // Damage the player if it is too close
+        Vector2 playerPosition = Player.transform.position;
+        float playerDistance = playerPosition.x + playerPosition.y - _currentFront;
+        // print(playerDistance);
+
+        if (playerDistance > 0)
+        {
+            return;
+        }
+        if (Player.GetComponent<AbstractEntityMovement>().Grounded)
+        {
+            if (playerDistance > -5)
+            {
+                Player.GetComponent<DamagableEntity>().ChangeHealth((int) playerDistance);
+            }
+            else
+            {
+                Player.GetComponent<DamagableEntity>().ChangeHealth(-5);
+            }
+        }
+    }
+}
